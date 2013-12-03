e7c9e0ae73ea55ebc59d3b1079135cea92caa2ea
Josh <wjosh60@yahoo.com>
2013-12-03 17:26:23 +0000

    Create index.php

---
+++ src/themes/index.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+<title>PixelCC - Access Denied</title>
+<style>
+@import url(http://fonts.googleapis.com/css?family=Lato);
+body {background-color:2ccef2;}
+ul
+{
+list-style-type:none;
+margin:0;
+padding:0;
+overflow:hidden;
+}
+li
+{
+display:inline;
+font-family: 'Lato', sans-serif;
+color:white;
+}
+A:link{
+color:#FE2E2E;
+text-decoration:none;
+}
+A:visited{
+color:#FE2E2E;
+text-decoration:none;
+}
+A:hover{
+color:#FE2E2E;
+text-decoration:underline;
+cursor:pointer;
+}
+h2 {
+background-color:#CEE3F6;
+box-shadow: 5px 5px 5px #888888;
+text-shadow: 3px 3px 30px #FF0000;
+}
+.content{
+border:0px solid black;
+background:white;
+width:985px;
+height:650px;
+padding:0px;
+position:static;
+top:8px;
+left:250px;
+border-radius:25px;
+}
+</style>
+</head>
+<center>
+<body>
+<div class="content">
+<center>
+<ul>
+<h2><li>| <a href="/">Home</a> |</li>
+<li><a href="http://pixelcc.byethost32.com/forum">Forums</a> |</li>
+<li><a href="http://pixelcc.byethost32.com/blog/">Blog</a> |</li>
+<li><a href="http://pixelcc.byethost32.com/forum/entry/register?Target=categories%2Fdiscussions">Register</a> |</li>
+<li><a href="http://pixelcc.byethost32.com/forum/entry/signin?Target=categories%2Fdiscussions">Sign In</a> |</li></h2><p>
+</center>
+</ul>
+
+<a href="http://pixelcc.byethost32.com/"><img src="http://i.imgur.com/6cFeJ0v.png"></a></p>
+
+<span style="font-family: 'Lato', sans-serif;font-size:30px;">Not allowed to view this directory :(</span>
+
+</div>
+</body>
+</center>
+</html>
